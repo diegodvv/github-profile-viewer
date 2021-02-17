840220f27f70da2360ddbab7b0abd35358906326
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-16 23:05:43 -0300

    Start to structure page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Heading } from '@chakra-ui/react';
+import { Flex, Grid, Heading } from '@chakra-ui/react';
 import { ThemeContainer } from './theme/ThemeContainer';
 
 function App() {
   return (
     <ThemeContainer>
-      <Heading as='h1'>Hello World!</Heading>
+      <Grid
+        as='main'
+        height='100vh'
+        templateColumns='1fr 1fr 1fr 1fr'
+        templateRows='1fr 1fr 1fr 1fr'
+        justifyContent='center'
+        alignItems='flex-start'
+        templateAreas="
+          '. . . .'
+          '. profile repositories .'
+          '. . . .'
+        ">
+        <Flex gridArea='profile' flexDir='column' alignItems='flex-start'>
+          <Heading as='h1'>Hello Left!</Heading>
+        </Flex>
+        <Flex gridArea='repositories' flexDir='column' alignItems='flex-start'>
+          <Heading as='h1'>Hello Right!</Heading>
+        </Flex>
+      </Grid>
     </ThemeContainer>
   );
 }
